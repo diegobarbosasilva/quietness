e3847e459211da6a39aff8dc0f93f2991e43bf32
diegobarbosasilva <iodatadiego@gmail.com>
2023-10-12 15:51:07 -0300

    111. Assign New Value at Index Position

---
+++ Python/02/12_lists_mutation/111_assign_new_value_at_index_position/1.py
@@ -0,0 +1,11 @@
+crayons = ["Macaroni and Cheese", "Maximum Yellow Red", "Jazzberry Jam"]
+
+print(crayons)
+
+crayons[1] = "Cotton Candy"
+
+print(crayons)
+
+crayons[-1] = "Aquamarine"
+
+print(crayons)
